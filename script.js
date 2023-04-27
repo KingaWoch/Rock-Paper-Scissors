6b2e100ad442744dc053644cd2a3b768adc011cd
Kinga Woch <kinga-niecko@o2.pl>
2023-04-27 09:59:07 +0200

    js state

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+// STATE
+
+const playerWinsLSKey = "playerWins";
+const houseWinsLSKey = "houseWins";
+
+let state = {
+  playerWins: localStorage.getItem(playerWinsLSKey) || 0,
+  houseWins: localStorage.getItem(houseWinsLSKey) || 0,
+  playerPick: null,
+  housePick: null,
+};
+
+const renderScore = () => {
+  const scoreNumber = document.querySelector(".score-number");
+  scoreNumber.innerText = state.playerWins - state.houseWins;
+};
+
+const getPickedButton = () => {
+  document.querySelectorAll(".button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      pickedByPlayer(e.currentTarget.dataset.pick);
+      pickedByHouse();
+      console.log(state);
+    });
+  });
+};
+
+const pickedByPlayer = (pickedButton) => {
+  state = {
+    ...state,
+    playerPick: pickedButton,
+  };
+};
+
+const pickedByHouse = () => {
+  const options = ["rock", "paper", "scissors"];
+  const housePick = options[Math.floor(Math.random() * options.length)];
+
+  state = {
+    ...state,
+    housePick,
+  };
+};
+
+const init = () => {
+  renderScore();
+  getPickedButton();
+};
+
+init();
+
+// DISPLAY RULES
+
 const rulesBtn = document.querySelector(".rules-btn");
 const rulesModal = document.querySelector(".rules-modal");
 const closeModalBtn = document.querySelector(".close-modal");
